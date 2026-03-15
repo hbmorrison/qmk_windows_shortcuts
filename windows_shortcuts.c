807b34f226082fea2209cb2c1f1055a6ff7fab22
Hannah Morrison <139557138+hbmorrison@users.noreply.github.com>
2026-03-15 20:44:15 +0000

    Comment

--- windows_shortcuts.c
+++ windows_shortcuts.c
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Include the standard QMK headers.
+
 #include QMK_KEYBOARD_H
 
 // True if Windows alt-tab behaviour is being processed.
